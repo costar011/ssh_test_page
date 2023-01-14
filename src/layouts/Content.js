5fdc655a2dbaf68cb5fbc5e50afc774ba0fb037f
costar011 <dnaer456@gmail.com>
2023-01-14 22:38:28 +0900

    test react

---
+++ src/layouts/Content.js
@@ -0,0 +1,14 @@
+import react from "react";
+import "./Content.css";
+
+
+const Content = () => {
+    return (
+        <div>
+            <div class="Content">test</div>
+        </div>
+    )
+}
+
+
+export default Content;
