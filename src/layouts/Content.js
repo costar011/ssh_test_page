00c2e6eb2ff0c7d5af345167c88d828e330468d3
costar011 <dnaer456@gmail.com>
2023-01-17 21:14:18 +0900

    box

--- src/layouts/Content.js
+++ src/layouts/Content.js
@@ -1,14 +1,15 @@
 import react from "react";
 import "./Content.css";
 
-
 const Content = () => {
-    return (
-        <div>
-            <div class="Content">test</div>
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <div class="Content">
+        <div className="center_mainbox"></div>
+        <div className="center_box"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Content;
